2538c1807ce751cb2770191a01b71685a4dd9fdb
lpcguest <lpcguest@PPD-118537.dhcp.fnal.gov>
2013-11-18 13:47:47 -0600

    add setup script

---
+++ FirmwarePatterns/setup_xilinx.sh
@@ -0,0 +1,7 @@
+#/cygdrive/c/Xilinx/14.4/ISE_DS/ISE/bin/
+
+PATH=/usr/local/bin:/usr/bin:/bin:/cygdrive/c/Xilinx/14.4/ISE_DS/ISE/bin/nt
+PATH=$PATH:/cygdrive/c/Xilinx/14.4/ISE_DS/ISE/lib/nt:/cygdrive/c/Windows/system32:/cygdrive/c/Windows:
+XILINX=C:/Xilinx/14.4/ISE_DS/ISE
+XILINXD_LICENSE_FILE=2700@ppd-acme
+LM_LICENSE_FILE=2700@ppd-acme
